eff155115672c8bf7a0f209a5f777c518641340d
nfabbro-reciprocity <nicolas.fabbro@reciprocitylabs.com>
2022-04-26 14:38:08 +0200

    clean up

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,6 +1,19 @@
+import AddIcon from '@mui/icons-material/Add';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Header from '../components/Header';
+import Container from '../components/Container';
+
 const List = () => {
   return (
-    <div>List</div>
+    <>
+      <Header
+        leftElement={<LogoutIcon />}
+        rightElement={<AddIcon />}
+      />
+      <Container className="list">
+        This is the list
+      </Container>
+    </>
   )
 }
 
--- src/pages/WalletConnection/WalletConnection.tsx
+++ src/pages/WalletConnection/WalletConnection.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
 import Header from '../../components/Header';
 import Container from '../../components/Container';
 import "./styles.scss";
@@ -8,7 +7,7 @@ import "./styles.scss";
 const WalletConnection = () => {
   return (
     <>
-      <Header rightElement={<AddIcon />} />
+      <Header />
       <Container className="wallet-connection">
         <Button variant="contained">Connect MetaMask</Button>
       </Container>
